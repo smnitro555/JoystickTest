af044d0d9e2a1b8210a809b0e0e1ca00a15b7889
smnitro555 <smnitro555@gmail.com>
2015-07-17 17:10:08 -0700

    Added Drone Control Class - Began Buildout to take in values

--- app/src/main/java/coolstuff/joysticktest/DroneControl.java
+++ app/src/main/java/coolstuff/joysticktest/DroneControl.java
@@ -4,4 +4,24 @@ package coolstuff.joysticktest;
  * Created by smnitro on 7/17/2015.
  */
 public class DroneControl {
+    private int motor1, motor2, motor3, motor4;
+    private int axisX = 0;
+    private int axisY = 0;
+    private int axisZ = 0;
+
+    public DroneControl() {
+        motor1 = 0;
+        motor2 = 0;
+        motor3 = 0;
+        motor4 = 0;
+    }
+
+    public void interpretString(String incoming) {
+        // String transmitted as 3 digits X value, 3 digits y value, 3 digits z value
+        axisX = Integer.parseInt(incoming.substring(0,2));
+        axisY = Integer.parseInt(incoming.substring(3,5));
+        axisZ = Integer.parseInt(incoming.substring(6,8));
+    }
+
+
 }
