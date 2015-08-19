947e9a008a2b30d49ad2eaee382807c4db7550cb
smnitro555 <smnitro555@gmail.com>
2015-08-19 19:41:39 -0400

    Clean Up for a new Test

--- app/src/main/java/coolstuff/joysticktest/MainActivity.java
+++ app/src/main/java/coolstuff/joysticktest/MainActivity.java
@@ -3,18 +3,36 @@ package coolstuff.joysticktest;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothSocket;
+import android.bluetooth.BluetoothDevice;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends Activity {
 
-    private JoystickView stickLeft , stickRight;
-    private TextView x1, x2, y1, y2;
-    private BluetoothAdapter BA;
+    JoystickView stickLeft , stickRight;
+    TextView x1, x2, y1, y2;
+    BluetoothAdapter myBluetoothAdapter;
+    BluetoothSocket myBluetoothSocket;
+    BluetoothDevice myBluetoothDevice;
+    OutputStream myOutputStream;
+    InputStream myInputStream;
+    Thread workerThread;
+    byte[] readBuffer;
+    int readBufferPosition;
+    int counter;
+    volatile boolean stopWorker;
+
     int REQUEST_ENABLE_BT = 1;
 
     public View findViewById(int id) {
@@ -32,12 +50,6 @@ public class MainActivity extends Activity {
         x2 = (TextView) findViewById(R.id.x2);
         y2 = (TextView) findViewById(R.id.y2);
 
-        BA = BluetoothAdapter.getDefaultAdapter();
-
-        if (!BA.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
 
         stickLeft.setOnJoystickMoveListener(
                 new JoystickView.OnJoystickMoveListener() {
@@ -83,9 +95,160 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void ConnectDevice(View view) {
-        String temp = "Device connected";
-        Toast.makeText(this, temp, Toast.LENGTH_SHORT).show();
+    //Open Button
+    openButton.setOnClickListener(new View.OnClickListener()
+    {
+        public void onClick(View v)
+        {
+            try
+            {
+                findBT();
+                openBT();
+            }
+            catch (IOException ex) { }
+        }
+    });
+
+    //Send Button
+    sendButton.setOnClickListener(new View.OnClickListener()
+    {
+        public void onClick(View v)
+        {
+            try
+            {
+                sendData();
+            }
+            catch (IOException ex) { }
+        }
+    });
+
+    //Close button
+    closeButton.setOnClickListener(new View.OnClickListener()
+    {
+        public void onClick(View v)
+        {
+            try
+            {
+                closeBT();
+            }
+            catch (IOException ex) { }
+        }
+    });
+}
+
+    void findBT()
+    {
+        myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(myBluetoothAdapter == null)
+        {
+            Toast.makeText(this, "No bluetooth adapter available", Toast.LENGTH_SHORT).show();
+        }
+
+        if(!myBluetoothAdapter.isEnabled())
+        {
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetooth, 0);
+        }
+
+        Set<BluetoothDevice> pairedDevices = myBluetoothAdapter.getBondedDevices();
+        if(pairedDevices.size() > 0)
+        {
+            for(BluetoothDevice device : pairedDevices)
+            {
+                if(device.getName().equals("HC-06"))
+                {
+                    myBluetoothDevice = device;
+                    break;
+                }
+            }
+        }
+        Toast.makeText(this, "Bluetooth Device Found", Toast.LENGTH_SHORT).show();
+    }
+
+    void openBT() throws IOException
+    {
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); //Standard SerialPortService ID
+        myBluetoothSocket = myBluetoothDevice.createRfcommSocketToServiceRecord(uuid);
+        myBluetoothSocket.connect();
+        myOutputStream = myBluetoothSocket.getOutputStream();
+        myInputStream = myBluetoothSocket.getInputStream();
+
+        beginListenForData();
+
+        Toast.makeText(this, "Bluetooth Opened", Toast.LENGTH_SHORT).show();
+    }
+
+    void beginListenForData()
+    {
+        final Handler handler = new Handler();
+        final byte delimiter = 10; //This is the ASCII code for a newline character
+
+        stopWorker = false;
+        readBufferPosition = 0;
+        readBuffer = new byte[1024];
+        workerThread = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                while(!Thread.currentThread().isInterrupted() && !stopWorker)
+                {
+                    try
+                    {
+                        int bytesAvailable = myInputStream.available();
+                        if(bytesAvailable > 0)
+                        {
+                            byte[] packetBytes = new byte[bytesAvailable];
+                            myInputStream.read(packetBytes);
+                            for(int i=0;i<bytesAvailable;i++)
+                            {
+                                byte b = packetBytes[i];
+                                if(b == delimiter)
+                                {
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    final String data = new String(encodedBytes, "US-ASCII");
+                                    readBufferPosition = 0;
+
+                                    handler.post(new Runnable()
+                                    {
+                                        public void run()
+                                        {
+                                            Toast.makeText(getApplicationContext(), data, Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                                }
+                                else
+                                {
+                                    readBuffer[readBufferPosition++] = b;
+                                }
+                            }
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        stopWorker = true;
+                    }
+                }
+            }
+        });
+
+        workerThread.start();
+    }
+
+    void sendData() throws IOException
+    {
+        String msg = myTextbox.getText().toString();
+        msg += "\n";
+        myOutputStream.write(msg.getBytes());
+    }
+
+    void closeBT() throws IOException
+    {
+        stopWorker = true;
+        myOutputStream.close();
+        myInputStream.close();
+        myBluetoothSocket.close();
+        Toast.makeText(getApplicationContext(), "Bluetooth Closed", Toast.LENGTH_SHORT).show();
     }
 
 }
